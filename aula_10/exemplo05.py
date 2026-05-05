aeaa2b9a391e58864f4eb235bf9ccc75f4aaaf33
ROBERT REYNOLDS RODRIGUEZ FLORES <robert.flores@aluno.senac.rio>
2026-05-05 20:24:03 -0300

    Adicionando EXCEPTION QUE É MAIS GERAL PARA OS ERROS

---
+++ aula_10/exemplo05.py
@@ -0,0 +1,19 @@
+#Cálculo da produtividade
+#-------------------------------------
+print('=== Cálculo de Produtividade ===')
+
+try:
+    total_produzido = float(input('Valor total da venda: '))
+    funcionarios = int(input('Total de funcionários: '))
+    
+    media_por_funcionario = total_produzido / funcionarios
+    
+except Exception as e:
+    print(f'Ops! Erro nos valores de entrada: {e}')
+except KeyboardInterrupt:
+    print('Operação cancelada pelo usuario.')
+else:
+    print(f'Média por funcionario: {media_por_funcionario:.2f}')
+# O finally é executado mesmo se dar ou não dar erro.
+finally:
+    print('Programa encerrado!')
